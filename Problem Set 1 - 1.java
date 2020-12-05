3abc42d4979a1f1a73b53071e03a403057bf6a81
Sina Ghaseminejad <sina.qane@yahoo.com>
2020-12-05 09:11:46 +0330

    Update Problem Set 1 - 1.java
    Edited comments

--- Problem Set 1 - 1.java
+++ Problem Set 1 - 1.java
@@ -20,19 +20,19 @@ public class Main {
         for (int i = 0; i < number_of_moves; i++) {
             if (String.valueOf(moves.charAt(i)).equals("U")) {
                 if (!(x % 2 == 0 && y % 3 == 2)) { // We mustn't hit x = 2*s and y = 3*t squares. So in x = 2*s and y = 3*t-1 squares, we mustn't move Up.
-                    if (y != n) { // We should not go out of the table
+                    if (y != n) { // We mustn't go out of the table
                         y += 1;
                     }
                 }
             } else if (String.valueOf(moves.charAt(i)).equals("R")) {
                 if (!(x % 2 == 1 && y % 3 == 0)) { // We mustn't hit x = 2*s and y = 3*t squares. So in x = 2*s + 1 and y = 3*t squares, we mustn't move right.
-                    if (x != m) { // We should not go out of the table
+                    if (x != m) { // We mustn't go out of the table
                         x += 1;
                     }
                 }
             } else if (String.valueOf(moves.charAt(i)).equals("D")) {
                 if (!(x % 2 == 0 && y % 3 == 1)) { // We mustn't hit x = 2*s and y = 3*t squares. So in x = 2*s and y = 3*t+1 squares, we mustn't move down.
-                    if (y != 1) { // We should not go out of the table
+                    if (y != 1) { // We mustn't go out of the table
                         y -= 1;
                     }
                 }
