cd872387d1e0e8174f332b5c953e478ef64f32c2
Sina Ghaseminejad <sina.qane@yahoo.com>
2020-12-05 09:23:00 +0330

    Update Problem Set 1 - 3.java
    Added comments

--- Problem Set 1 - 3.java
+++ Problem Set 1 - 3.java
@@ -18,8 +18,11 @@
     
             String ans = "";
     
+            // In case that two string were alike up to the last character of one of them, we need to put the smaller string as the answer.
+            // e.g. str1 = "foo" and str2 = "foobar". In this case the answer is str1 = "foo".
+            
             int min = 0;
-            if (len1 < len2) {
+            if (len1 < len2) { 
                 min = len1;
                 ans = str1;
             } else {
@@ -29,18 +32,19 @@
     
             int i = 0;
             while (i < min) {
-                String char1 = String.valueOf(str1.charAt(i));
-                String char2 = String.valueOf(str2.charAt(i));
+                String char1 = String.valueOf(str1.charAt(i)); // The i-th characher of str1
+                String char2 = String.valueOf(str2.charAt(i)); // The i-th characher of str2
                 int j = 0;
-                while (j < len_alpha) {
+                while (j < len_alpha) { // Looping through the alphabet in order to find the place of char1 and char2.
                     if (String.valueOf(alpha.charAt(j)).equals(char1)) {
-                        temp1 = j;
+                        temp1 = j; // The index of str1's i-th character in our given alphabetical order.
                     }
                     if (String.valueOf(alpha.charAt(j)).equals(char2)) {
-                        temp2 = j;
+                        temp2 = j; // The index of str2's i-th character in our given alphabetical order.
                     }
                     j++;
                 }
+                // If two characters we not alike, then the asnwer is the string with the smaller value for it's i-th character's index.
                 if (temp1 < temp2) {
                     ans = str1;
                     break;
@@ -50,7 +54,7 @@
                 }
                 i++;
             }
-    
+            // The conversions asked in the problem. Nothing serious...
             String upper = String.valueOf(ans.charAt(0)).toUpperCase() + ans.substring(1);
             String reverse = new StringBuffer(upper).reverse().toString();
             System.out.println(upper + reverse);
