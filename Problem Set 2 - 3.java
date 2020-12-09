4829a73272924c8419fe66cf49a0e1f345da0ecc
Sina Ghaseminejad <sina.qane@yahoo.com>
2020-12-09 13:17:52 +0330

    Update Problem Set 2 - 3.java
    Added two better solutions than mine by Nima Amoey and Negin Lotfi.

--- Problem Set 2 - 3.java
+++ Problem Set 2 - 3.java
@@ -1,4 +1,118 @@
+// Solution 1. (By Nima Amoey)
+
 import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+        int x = (int) Math.pow(2, (n / 2) + 2) - 1;
+        String[][] str = new String[x][x];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                str[i][j] = ".";
+            }
+        }
+        for (int j = 1; Math.pow(2, j) < x / 2 + 2; j++) {
+            int k = x / 2 - (int) Math.pow(2, j) + 1;
+            for (int l = k; l < x / 2 + 1; l++) {
+                str[k][l] = "#";
+                str[l][k] = "#";
+            }
+            for (int l = k, h = k; l < x / 2 + 1 && h >= 0; l++, h--) {
+                str[l][h] = "#";
+                str[h][l] = "#";
+            }
+        }
+        for (int i = 0; i < x / 2 + 1; i++) {
+            for (int j = 0; j < x / 2; j++) {
+                str[i][x - 1 - j] = str[i][j];
+            }
+        }
+        for (int i = 0; i < x / 2; i++) {
+            for (int j = 0; j < x; j++) {
+                str[x - 1 - i][j] = str[i][j];
+            }
+        }
+        if (n % 2 == 0) {
+            str[x / 2][x / 2 - 1] = ".";
+            str[x / 2][x / 2 + 1] = ".";
+            str[x / 2 - 1][x / 2] = ".";
+            str[x / 2 + 1][x / 2] = ".";
+        }
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.print(str[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+}
+
+// Solution 2. (By Negin Lotfi)
+
+/* import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String[][] str = new String[1024][1024];
+        double m = Math.pow(2, Math.floor(n / 2) + 2) - 1;
+        for (int i = 0; i < Math.pow(2, Math.floor(n / 2) + 2) - 1; i++) {
+            for (int j = 0; j < Math.pow(2, Math.floor(n / 2) + 2) - 1; j++) {
+                str[i][j] = ".";
+            }
+        }
+        for (int i = 0; i < Math.pow(2, Math.floor(n / 2) + 2) - 1; i++) {
+            str[0][i] = "#";
+            str[(int) m -1][i] = "#";
+            str[i][0] = "#";
+            str[i][(int) m-1] = "#";
+        }
+        int tavan = 4;
+        int y = 0;
+        for(int i = 1;i<=Math.ceil((n-1)/2);i++) {
+            y += (int) Math.ceil(m / tavan);
+            str[y][y] = "#";
+            tavan *= 2;
+            for (int j = y; j < m - y; j++) {
+                str[y][j] = "#";
+                str[(int) m - y-1][j] = "#";
+                str[j][y] = "#";
+                str[j][(int) m - y-1] = "#";
+            }
+        }
+        int tavan1 = 2;
+        int z = 1;
+        int sum = 0;
+        for (int i = 1; i <= (n / 2); i++) {
+            sum += Math.ceil(m / tavan1);
+            for (int j = z; j <= (m / 2); j++) {
+                str[sum - j][j] = "#";
+                str[sum - j][(int) m - j - 1] = "#";
+                str[(int) m - sum - 1 + j][j] = "#";
+                str[(int) m - sum - 1 + j][(int) m - j - 1] = "#";
+            }
+            tavan1 *= 2;
+            z += m / tavan1 + 1;
+
+
+        }
+
+        for (int i = 0; i < Math.pow(2, Math.floor(n / 2) + 2) - 1; i++) {
+            for (int j = 0; j < Math.pow(2, Math.floor(n / 2) + 2) - 1; j++) {
+                System.out.print(str[i][j]);
+            }
+            System.out.println();
+        }
+    }
+} */
+
+
+// Solution 3. (By Sina Ghaseminejad)
+
+/*  import java.util.Scanner;
 
 public class Main {
 
@@ -121,4 +235,4 @@ public class Main {
             Print(tool, A);
         }
     }
-}
+} */
